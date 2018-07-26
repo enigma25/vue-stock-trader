82142ee0732029da4de74e59f86bc3c6540851eb
Enigma25 <enigma25@gmail.com>
2018-07-26 17:54:47 +0200

    adding logo to the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
         <h1>Welcome super trader</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aut autem, blanditiis consequuntur
-            dolore excepturi laborum maiores minima nihil non nulla obcaecati quas quibusdam quod sed suscipit vero
-            voluptatem.</p>
-        <p class="font-italic text-info">Your funds : {{ funds | currency}}</p>
+        <p class="font-italic text-info">
+            Your funds : {{ funds | currency}}
+        </p>
+        <div  class="logo-container">
+          <img src="../assets/logo.svg" alt="logo" id="logo"/>
+        </div>
     </div>
 </template>
 
@@ -17,5 +19,15 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.logo-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+#logo {
+  display: block;
+  height: 50%;
+  width: 40%;
+}
 </style>
