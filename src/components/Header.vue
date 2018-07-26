7a8ecad03e21766bf132d9562e2839a20fc2265e
Enigma25 <enigma25@gmail.com>
2018-07-26 18:01:26 +0200

    fixing navbar breakpoint

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="collapse navbar-collapse">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbarSm">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsingNavbarSm">
         <ul class="navbar-nav mr-auto">
           <router-link 
             to="/" 
